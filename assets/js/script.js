37a735ef13034d867795b0e16cc7e565697d652d
ryanp <rdpodols@gmail.com>
2022-03-28 22:56:41 -0400

    code template

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// zen quotes documentation - https://premium.zenquotes.io/zenquotes-documentation/
+
+var mailChimpApiKey = "15983a1116a19961b677deabe1852fd6";
+var zenQuotesUrl = "https://zenquotes.io/api/quotes";
+
