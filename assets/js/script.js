05c8cd5ccede9812656e63aa6d5cb8b408f52a82
ryanp <rdpodols@gmail.com>
2022-03-30 20:44:48 -0400

    fixed links for image file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
-// zen quotes documentation - https://premium.zenquotes.io/zenquotes-documentation/
-
 var mailChimpApiKey = "15983a1116a19961b677deabe1852fd6";
-var zenQuotesUrl = "https://zenquotes.io/api/quotes";
+var zenQuotesUrl = "https://type.fit/api/quotes";
+
+
+var getZenQuotes = function() {
+    fetch(zenQuotesUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data){
+                console.log(data);
+                //var quote = (data[0].q);
+                //var quoteText = $("<p>").text(quote);
+                //$("#quote").append(quoteText);
+            })
+        }else{console.log("nope")}
+    })
+};
+
+getZenQuotes();
+
+$.ajax({
+    type:'POST',
+    url:'https://aztro.sameerkumar.website?sign=aries&day=today',
+    success:function(data){
+    console.log(data);
+    }
+     });
+
 
